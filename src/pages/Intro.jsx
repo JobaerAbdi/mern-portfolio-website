ae060ee1369468285e2044445d135e0a68205f74
JobaerAbdi <jobaerrahman757@gmail.com>
2024-02-29 20:31:06 +0600

    Intro  section responsive completed

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -2,13 +2,13 @@ const Intro = () => {
   return (
     <div className="h-[80vh] bg-primary flex flex-col items-start p-3 justify-center gap-8 py-10">
       <h1 className="text-white">Hi, I am</h1>
-      <h1 className="text-secondary text-5xl font-semibold">
+      <h1 className="text-secondary text-5xl sm:text-2xl font-semibold">
         Md Jobaer Rahman
       </h1>
-      <h1 className="text-white text-4xl font-semibold">
+      <h1 className="text-white text-4xl sm:text-2xl font-semibold">
         I build think for the web
       </h1>
-      <p className="text-white w-2/3">
+      <p className="text-white w-2/3 sm:text-sm">
         I am MERN stack Web Developer. Currently i am working as a React
         Developer. Also sharing my knowledge to the students that I have gained
         in my carrier through across the world.{" "}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "primary": "#0a192f",
-        "secondary": "#f97316",
-        "tertiary": "#54d688"
-      }
+        primary: "#0a192f",
+        secondary: "#f97316",
+        tertiary: "#54d688",
+      },
+    },
+    screens: {
+      lg: { max: "2023px" },
+      sm: { max: "639px" }
     },
   },
   plugins: [],
-}
-
+};
