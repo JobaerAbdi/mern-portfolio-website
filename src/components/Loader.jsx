110844153df4e8aa75c868fea4013423224fc06e
JobaerAbdi <jobaerrahman757@gmail.com>
2024-03-01 21:00:55 +0600

    Animation part complete

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+      <div className="flex justify-center items-center fixed inset-0 bg-primary gap-6 ">
+        <h1 className="text-secondary font-bold text-4xl m">M</h1>
+        <h1 className="text-white font-bold text-4xl j">J</h1>
+        <h1 className="text-tertiary font-bold text-4xl r">R</h1>
+      </div>
+  );
+};
+
+export default Loader;
